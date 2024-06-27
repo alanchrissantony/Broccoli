36840f97e1cbdfc4f16d830dceca7f834f0ea274
alanchrissantony <alanchrissantony@gmail.com>
2024-06-27 21:19:45 +0530

    vercel

--- broccoli/settings.py
+++ broccoli/settings.py
@@ -156,7 +156,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 # Media files configuration
 
