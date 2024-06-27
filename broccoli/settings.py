1b1c0b19deded0c6d8f9d6048923f43b6f5f14a5
alanchrissantony <alanchrissantony@gmail.com>
2024-06-27 20:11:23 +0530

    vercel

--- broccoli/settings.py
+++ broccoli/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 PAYPAL_CLIENT_ID = config('PAYPAL_CLIENT_ID')
 PAYPAL_CLIENT_SECRET = config('PAYPAL_CLIENT_SECRET')
