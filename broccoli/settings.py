2f31842f5f4cd4dbb9f535b590a0190b92ded52c
alanchrissantony <alanchrissantony@gmail.com>
2024-07-02 00:27:32 +0530

    vercel deploy

--- broccoli/settings.py
+++ broccoli/settings.py
@@ -172,3 +172,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+SECURE_HSTS_SECONDS = 3600
+SECURE_SSL_REDIRECT = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+X_FRAME_OPTIONS = 'DENY'
