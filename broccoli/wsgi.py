84baf9842ce615992c0956baa5dff5c47ad7e5e4
alanchrissantony <alanchrissantony@gmail.com>
2024-07-24 06:18:16 +0530

    build files

--- broccoli/wsgi.py
+++ broccoli/wsgi.py
@@ -15,3 +15,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'broccoli.settings')
 
 
 application = get_wsgi_application()
+app = application
---
+++ build_files.sh
@@ -0,0 +1,3 @@
+pip3 install -r requirements.txt
+python3 manage.py collectstatic --no-input
+python3 manage.py migrate
